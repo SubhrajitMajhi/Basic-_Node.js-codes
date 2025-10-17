62dcc39d775b14e375ab94878c352f2b9c6c2f44
SubhrajitMajhi <subhrajitmajhiz1j9@gmail.com>
2025-10-17 09:51:01 +0530

    file updated

--- sorting12.js
+++ sorting12.js
@@ -1,12 +1,14 @@
 //sorting in array from 1 to n
-function sortArray(arr){
-    let arr = [1, 9, 2, 4, 5]
-    arr.sort((a,b)=>a-b)
+function sortArray() {
+    let arr = [1, 9, 2, 4, 5];
+    arr.sort((a, b) => a - b);
 
     process.stdout.write("Sorted array: ");
-    arr.forEach(num => process.stdout.write(num, " "))
+    arr.forEach(num => process.stdout.write(num + " "));
 }
+
 sortArray();
+
 
 //.length
 //.tostring
